8b2ec67da4b329759adb64f184c59e47952c7936
muskaan-0908 <muskaanmakkar987@gmail.com>
2025-12-16 03:45:34 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (11.82%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n<=0) return false;
+    
+    
+    return n > 0 && 1162261467 % n == 0;
+}
 
-        while(n%3==0) {
-            n=n/3;
-        }
-        return (n==1);
-    }
+    //     if (n<=0) return false;
+
+    //     while(n%3==0) {
+    //         n=n/3;
+    //     }
+    //     return (n==1);
+    // }
 };
