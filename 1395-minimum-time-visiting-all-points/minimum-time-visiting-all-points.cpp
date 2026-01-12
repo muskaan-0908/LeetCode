7498372cd3ce0feca6e3ae1104ecb18231b3f6b3
muskaan-0908 <muskaanmakkar987@gmail.com>
2026-01-13 00:00:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (61.26%) - LeetSync

---
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+                int time = 0;
+
+        for (int i = 0; i < points.size() - 1; i++) {
+            time = time + max(
+                abs(points[i+1][0] - points[i][0]),
+                abs(points[i+1][1] - points[i][1])
+            );
+        }
+
+        return time;
+    }
+};
